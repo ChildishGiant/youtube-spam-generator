50b34b637d52c7b0946100f8e955c95b261753bf
SolarPolarMan <Solarpolarman@gmail.com>
2016-08-26 18:13:27 +0100

    done for today

---
+++ jssource.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  gen();
+});
+
+$(document).keypress(function(e) {
+  gen();
+});
+
+
+//intensifiers[Math.floor(Math.random() * intensifiers.length)]);
+
+//░░░░░░███████ ]▄▄▄▄▄▄▄▄ Bob is building an army.
+//▂▄▅█████████▅▄▃▂ ☻/ This tank & Bob are against Google+
+//Il███████████████████]. /▌ Copy and Paste this all over
+//◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤.. / \ This website if you are with us﻿.
+//
+//
+//
+//▶SteamWallet▶PSNCodes▶XboxCodes▶uTubesCodes▶GooglePlayCodes▶ClashOfClansGEMS▶AmazonCodes▶RIOTPoints▶FIFAWorldCoins▶MinecraftPremium▶iPhone 6▶
+//
+//
+//
+
+
+
+function gen(){
+  console.log("lkhgej")
+  //commentGen();
+  //userGen();
+  //likeGen();
+  //imageGen();
+}
