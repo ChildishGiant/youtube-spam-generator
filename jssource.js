22461575c24c978fe0cb982b706bc01c726099a1
ChildishGiant <ChildishGiant@gmail.com>
2017-10-27 09:03:00 +0100

    Nah

--- jssource.js
+++ jssource.js
@@ -15,13 +15,50 @@ $(document).keypress(function(e) {
 //◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤.. / \ This website if you are with us﻿.
 //
 //
-//
 //▶SteamWallet▶PSNCodes▶XboxCodes▶uTubesCodes▶GooglePlayCodes▶ClashOfClansGEMS▶AmazonCodes▶RIOTPoints▶FIFAWorldCoins▶MinecraftPremium▶iPhone 6▶
 //
+//＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿
+//| Windows Dialog　　　　　　　　　　　　　　　　　　    [－] [口] [×]   |
+//| ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣ ￣ |
+//|　Windows has detected that this song is awesome!                                  |
+//|     Do you agree? 　　　　　　                                                                        |
+//|　 　　＿＿＿＿＿＿　　　　＿＿＿＿＿＿　　　　＿＿＿＿＿　　      |
+//| 　 　｜　Yes　　     |　　　｜　Maybe　｜ 　 　      | 　   No　  |            |
+//|　 　　￣￣￣￣￣￣　　　　￣￣￣￣￣￣　　　　￣￣￣￣￣　          |
+//|＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿ __ _﻿
+//
+//who watch this on youtube 🙌﻿
+//Can you guys help me out to get 1,000 subs so I can get a dog. My mom said if I get 1k. Thanks like this comment please﻿
+//
+//if this comment gets 137 likes, there won't be school tomorrow
+//sub and watch my videos= your crush loves you and hugs you👏👋👌😘😍😍😘😘😘﻿
 //
 //
-
-
+//Thumbs this up so Casey can see this! I just posted some DJI Mavic drone footage on my channel. YOU HAD YOURS OUT OF FOCUS WHEN YOU DID THE COMPARISON!!
+//
+//Please consider mentioning it in your vlog so people are aware!﻿
+//
+//Like or your mom dies, subscribe and she lives for ever.﻿
+//
+//If u like this , you'll find 300000$ tomorrow!!!﻿
+//
+//Guys this is for all of you!
+//Go to:   t.co/bTUpUoJh9e?unique=D8NDNJS﻿
+//
+// Takе acссtion guys: useeе this link ...before thееу runs out !! Links Hеrе!! --- https://twitter.com/1be51d81642d4307b/status/7
+//
+//
+//USEЕEЕ ТHIS--> https://twitter.com/28b4d3e2579adfed1/status/754948765031632896 IТTTT IS THЕ WORKING Crоssy Road Hack ТOOL THAT WORKS! ENJOY! Crossy Rоаd AAAA Whalee оoоf а TТime﻿
+//
+//LOL guys, so just search for this video on YouTube: <b>Carter Dolan Simple Hack</b>
+//and thank me later on﻿
+//
+//
+//Once you start to read this comment you only have TEN seconds to like it or else THIS WEEK you'll find something you wish you had not.﻿
+//
+//1232 random people who will like this will get kissed by their crush tomorrow :)﻿
+//
+//∂σ үσυ ωαηт тσ ℓσss ωειgнт ιη נυsт 3 ωεεкs тнαη cнεcк συт тнιs : - http://lkn.li/NVUco?54592﻿
 
 function gen(){
   console.log("lkhgej")
